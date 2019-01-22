c5d2ac586d64fd0a32405f02a564020fd73545af
Eliot Krone <krondev@pm.me>
2019-01-22 22:20:27 +0300

    fix for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :api, TokenxApiWeb.Endpoint,
   # http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: "radiant-lake-30175.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
