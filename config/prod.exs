f7babebb932ad5606e1552c10c60f3479de9b7ed
Eliot Krone <krondev@pm.me>
2019-01-22 22:15:23 +0300

    fix for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :api, TokenxApiWeb.Endpoint,
   # http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: "https", host: "polar-stream-84768.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "radiant-lake-30175.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
   cache_static_manifest: "priv/static/cache_manifest.json"
