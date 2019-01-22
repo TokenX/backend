bad00e14d43cf8b967670e2a1fd6e5f9c6c0fa7c
Eliot Krone <krondev@pm.me>
2019-01-22 22:46:54 +0300

    bugfix

--- config/prod.exs
+++ config/prod.exs
@@ -69,7 +69,7 @@ config :logger, level: :info
 # See the releases documentation accordingly.
 
 # Configure your database
-config :hello, TokenxApi.Repo,
+config :api, TokenxApi.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
